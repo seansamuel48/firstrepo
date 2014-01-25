9bb68e53023dd775c3673f8fc113acca10309c27
Sean S <seansamuel_48@yahoo.com>
2014-01-24 21:28:46 -0500

    validator

--- test.js
+++ test.js
@@ -13,3 +13,6 @@ for(i=1; i<10; i++){
 console.log("Marty is the greatest cs person alive")
 
 console.log("I added something that better not show up on master")
+
+var val = require("validator")
+console.log("email is " + val.isEmail("foo@bar.com"))
